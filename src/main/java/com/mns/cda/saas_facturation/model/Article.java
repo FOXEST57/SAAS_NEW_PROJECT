13e048b9495c6a4c1d18b5175ce0acf3f11faf5b
Dorian Weber <dorianweb57@gmail.com>
2026-06-24 14:30:11 +0200

    feat: Validateur ajout des validateurs Srping sur les model Article et Tva pour bloquer les incohérences en base de données

--- src/main/java/com/mns/cda/saas_facturation/model/Article.java
+++ src/main/java/com/mns/cda/saas_facturation/model/Article.java
@@ -5,6 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,18 +21,27 @@ import lombok.Setter;
 @Entity
 public class Article {
 
+    //L'entité Article aucun champ ne peut être null ou vide quand c'est du String et pour les nombres ils ne peuvent pas être négatif ou = 0 pour le prix avant taxe
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long artId;
 
+    @NotBlank
     protected String artReference;
 
+    @NotBlank
     protected String artName;
 
+    @NotNull
     protected String artDescription;
 
+    @NotNull
+    @DecimalMin(value = "0.0", inclusive = false)
     protected Double artPriceExcludeTaxes;
 
+    @NotNull
+    @Min(0)
     protected int artStock;
 
 }
--- src/main/java/com/mns/cda/saas_facturation/model/Tva.java
+++ src/main/java/com/mns/cda/saas_facturation/model/Tva.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.DecimalMin;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,9 +17,12 @@ import lombok.Setter;
 @Entity
 public class Tva {
 
+    //L'entité TVA aucun champ les taux ne peuvent pas être négatif
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long tvaId;
 
+    @DecimalMin(value = "0.0")
     private Double tvaTaux;
 }
