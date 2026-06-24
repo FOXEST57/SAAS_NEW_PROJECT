4a4dab87ab49a215c0f2c41924f76b59a5eedbaa
Dorian Weber <dorianweb57@gmail.com>
2026-06-24 11:55:17 +0200

    initialisation projet

---
+++ src/main/java/com/mns/cda/saas_facturation/SaasFacturationApplication.java
@@ -0,0 +1,13 @@
+package com.mns.cda.saas_facturation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasFacturationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaasFacturationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mns/cda/saas_facturation/SaasFacturationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mns.cda.saas_facturation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaasFacturationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
