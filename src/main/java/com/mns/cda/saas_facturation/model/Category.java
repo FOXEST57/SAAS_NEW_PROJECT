09056854fc24396c03f98e98d49db7d6e055f170
Dorian Weber <dorianweb57@gmail.com>
2026-06-25 09:37:03 +0200

    feat(Category): ajout du model pour la table catégorie

---
+++ src/main/java/com/mns/cda/saas_facturation/model/Category.java
@@ -0,0 +1,27 @@
+package com.mns.cda.saas_facturation.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long catId;
+
+    @NotBlank
+    protected String catName;
+
+}
