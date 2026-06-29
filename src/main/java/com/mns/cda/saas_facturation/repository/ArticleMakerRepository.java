80ec381a4aa2f1ba48c6be1e6a0b349f8e33cf1f
Jeremy.Dev <jeremy.gay.dev@gmail.com>
2026-06-29 15:53:47 +0200

    feat: ajout ArticleMakerRepository

---
+++ src/main/java/com/mns/cda/saas_facturation/repository/ArticleMakerRepository.java
@@ -0,0 +1,8 @@
+package com.mns.cda.saas_facturation.repository;
+
+import com.mns.cda.saas_facturation.model.Maker;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleMakerRepository extends JpaRepository<Maker, Long> {
+
+}
