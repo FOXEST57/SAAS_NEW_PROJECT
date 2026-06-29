cd45cb36c1f1fb317ebad0b710a7a3d9167a8904
Jeremy.Dev <jeremy.gay.dev@gmail.com>
2026-06-29 14:24:23 +0200

    feat ajout entité ArticleMaker

--- src/main/java/com/mns/cda/saas_facturation/model/ArticleMaker.java
+++ src/main/java/com/mns/cda/saas_facturation/model/ArticleMaker.java
@@ -2,50 +2,45 @@ package com.mns.cda.saas_facturation.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.List;
 
-@Setter
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class ArticleSupplier {
+public class ArticleMaker {
 
     @Embeddable
     @AllArgsConstructor
     @NoArgsConstructor
-    public static class ArticleSupplierId implements Serializable {
+    public static class ArticleMakerId implements Serializable {
         @Column(name ="article_id")
         private Long articleId;
-        @Column(name = "supplier_id")
-        private Long supplierId;
+        @Column(name ="maker_id")
+        private Long makerId;
     }
 
     @EmbeddedId
-    private ArticleSupplierId artSplId;
+    private ArticleMakerId artMkrId;
 
     @ManyToOne
     @MapsId("articleId")
     @JoinColumn(name = "article_id")
     protected Article article;
 
-
     @ManyToOne
-    @MapsId("supplierId")
-    @JoinColumn(name = "supplier_id")
-    protected Supplier supplier;
+    @MapsId("makerId")
+    @JoinColumn(name ="maker_id")
+    protected Maker maker;
 
     @NotBlank
-    protected String artSplReference;
-
-    @NotNull
-    protected int artSplStock;
+    protected  String artMkrReference;
 
 }
