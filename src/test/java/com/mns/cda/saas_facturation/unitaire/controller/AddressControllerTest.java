b57b28c8f1955e864077af7fd31b8dbcfcb06afb
Dorian Weber <dorianweb57@gmail.com>
2026-07-10 09:58:02 +0200

    Test : ajout des tests pour le model de CustomerController

--- src/test/java/com/mns/cda/saas_facturation/unitaire/controller/AddressControllerTest.java
+++ src/test/java/com/mns/cda/saas_facturation/unitaire/controller/AddressControllerTest.java
@@ -11,8 +11,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
